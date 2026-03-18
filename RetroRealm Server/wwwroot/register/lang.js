a42d31297dd3f7b753e42d4cfd45fb2c22b4e28d
marci <lorincmarton3@gmail.com>
2026-03-18 11:53:52 +0100

    login és regisztráció v1

---
+++ RetroRealm Server/wwwroot/register/lang.js
@@ -0,0 +1,57 @@
+export const texts = {
+  loginH1: {
+    en: "RetroRealm Login",
+    hu: "RetroRealm Bejelentkezés",
+    esp: "Inicio de sesión en RetroRealm",
+  },
+  inputtext: {
+    en: "Username",
+    hu: "Felhasználónév",
+    esp: "Nombre de usuario",
+  },
+  inputpass: {
+    en: "Password",
+    hu: "Jelszó",
+    esp: "Contraseña",
+  },
+  belepes: {
+    en: "Login",
+    hu: "Belépés",
+    esp: "Acceso",
+  },
+  szin: {
+    en: "Don't have an account yet? Click <a id='link' href='registerlaptop2.html'>ide</a>",
+    hu: "Még nem regisztrált? Kattintson <a id='link' href='registerlaptop2.html'>ide</a>",
+    esp: "¿Aún no estás registrado? Haz clic <a id='link' href='registerlaptop2.html'>ide</a>",
+  },
+  link: {
+    en: "here!",
+    hu: "ide!",
+    esp: "aquí!",
+  },
+  nyelvvalt: {
+    en: "Language",
+    hu: "Nyelv",
+    esp: "Idioma",
+  },
+  registH1: {
+    en: "RetroRealm Registration",
+    hu: "RetroRealm Regisztráció",
+    esp: "Registro RetroRealm",
+  },
+  inputpassagain: {
+    en: "Password again",
+    hu: "Jelszó újra",
+    esp: "Contraseña nuevamente",
+  },
+  regisztracio: {
+    en: "Create account",
+    hu: "Fiók létrehozása",
+    esp: "Crear una cuenta",
+  },
+  szin2: {
+    en: "Already have an account? Click <a id='link' href='loginlaptop2.html'>ide</a>",
+    hu: "Már regisztrált? Kattintson <a id='link' href='loginlaptop2.html'>ide</a>",
+    esp: "¿Ya estás registrado? Haz clic <a id='link' href='loginlaptop2.html'>ide</a>",
+  },
+};
